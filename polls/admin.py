f22355d2ecbed79cf918ea1b4bc5a6aae5c36750
wendell1101 <wendellchansuazo11@gmail.com>
2020-04-02 17:50:41 +0800

    add pollapp

---
+++ polls/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from  .models import Question,Choice
+
+admin.site.site_header = 'Polls/Survey'
+admin.site.site_title = 'Polls Admin'
+admin.site.index_title = 'Polls Application'
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [(None, {'fields': ['question_text']}),
+                 ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}), ]
+    inlines = [ChoiceInline]
+
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+admin.site.register(Question, QuestionAdmin)
